3efda402b51c580ce72a21cbcdec4787f82d0266
Farhan Ali Mirza <farhanalimirza2003@gmail.com>
2025-08-08 04:10:40 +0530

    config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      "@typescript-eslint/no-unused-vars": 0,
+      "@typescript-eslint/no-explicit-any": 0,
+    }
+  },)
 ];
 
 export default eslintConfig;
